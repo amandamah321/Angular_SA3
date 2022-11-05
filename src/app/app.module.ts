afa23a6f4b0fad6318c09ad99eae156721e4b8e6
Amanda Maia <amanda_mah321@hotmail.com>
2022-11-05 13:11:22 +0100

    Aula de Interação API 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,7 +22,7 @@ import { FooterComponent } from './components/footer/footer.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user';
+import { LoginService } from 'src/app/service/login.service';
 
 @Component({
   selector: 'app-login',
@@ -8,15 +9,17 @@ import { User } from 'src/app/models/user';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private loginService: LoginService) { }
   ngOnInit(): void {
   }
 
   userModel = new User();
 
   receberDados() {
-    console.log(this.userModel);
+    
+    this.loginService.login(this.userModel).subscribe({
+      next: (Response) => {console.log()}
+    })
     
   }
 
